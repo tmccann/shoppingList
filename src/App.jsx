3da531addcf6d859a06c90181aa2988dc490eab5
tommy <tommy.mccann001@gmail.com>
2023-07-17 23:05:38 +0100

    mostly cosmetic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,69 @@
 import './App.css'
+import { useState } from "react";
 import AddItem from './assets/Components/AddItem/AddItem';
 import Header from './assets/Components/Header/Header';
 import ShoppingList from './assets/Components/ShoppingList/ShoppingList';
 
+
 const App = () => {
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: false,
+      item: "mushrooms with creamy garlic sauce monkey elephant cheese"
+    },
+
+    {
+      id: 2,
+      checked: false,
+      item: "beans"
+    },
+
+    {
+      id: 3,
+      checked: false,
+      item: "rice"
+    },
+
+    {
+      id: 4,
+      checked: false,
+      item: "pasta"
+    },
+
+
+  ])
+
+  const handleCheck = (id) => {
+    // use map to go through items and find matching id
+    const listItems = items.map((items) => items.id === id ? {
+      // when true load all items changing selected id's  check value to opposite value: ie true to false
+      ...items, checked: !items.checked
+    } : items);
+    // save new item state to state
+    setItems(listItems);
+    // save to local storage
+    localStorage.setItem('shoppingList', JSON.stringify(listItems));
+  }
+
+  const deleteItem = (id) => {
+    console.log(id)
+    // create new array with all items that do not equal id
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems)
+    localStorage.setItem('shoppingList', JSON.stringify(listItems));
+
+  }
   return (
-      <div className="wrap">
-        <Header />
-        <AddItem />
-        <ShoppingList /> 
-      </div>
+    <div className="wrap">
+      <Header />
+      
+      <ShoppingList
+          items={items}
+          handleCheck={handleCheck}
+          deleteItem={deleteItem}
+      />
+    </div>
   );
 };
 
--- src/assets/Components/AddItem/AddItem.jsx
+++ src/assets/Components/AddItem/AddItem.jsx
@@ -1,8 +1,14 @@
 import { FaPlus } from "react-icons/fa"
 import './addItem.css'
 
-function AddItem() {
+const AddItem = () => {
+
+  const addItem = (i) =>{
+    console.log(i)
+
+  }
   return (
+    <div className="shadowContainer">
     <form className='addItemForm'>
         <label htmlFor='addItem'>Add Item</label>
         <input
@@ -11,15 +17,19 @@ function AddItem() {
             type='text'
             placeholder='Add Item'
             required
+            // value={newItem}
+            // onChange={(e) => setnewItem(e.target.value)}
         />
         <button 
             type='submit'
             aria-label='Add Item'
+            onClick={addItem((i) => (i.value))}
         >
-        <FaPlus />
+        <FaPlus className="plus work"/>
         </button>
 
     </form>
+    </div>
   )
 }
 
